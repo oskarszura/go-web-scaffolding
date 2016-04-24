67e80b4d71671b5c12ce618eef9360264bc416bf
oskarszura <oskar.szura@gmail.com>
2016-04-24 11:34:49 +0200

    Split the project among packages

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
     "net/http"
     "log"
+    "fmt"
+    router "github.com/oskarszura/gowebrouter"
 )
 
 func main() {
+    router.Test()
     http.Handle("/", http.FileServer(http.Dir("./public")))
-    err := http.ListenAndServe(":9090", nil)
+    setupServer("9090")
+}
+
+func setupServer(port string) {
+    fmt.Println("Setting up server on " + port + " port")
+
+    fmt.Println("Listening...")
+    err := http.ListenAndServe(":" + port, nil)
 
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
