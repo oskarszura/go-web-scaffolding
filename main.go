07ea6553ece06e465687abf12f99e09b79d85b5e
oskarszura <oskar.szura@gmail.com>
2016-04-24 21:34:34 +0200

    Move logic to packages

--- main.go
+++ main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "fmt"
-    router "github.com/oskarszura/gowebrouter"
+    goWebServer "github.com/oskarszura/gowebserver"
 )
 
 func main() {
-    router.Test()
-    http.Handle("/", http.FileServer(http.Dir("./public")))
-    setupServer("9090")
-}
-
-func setupServer(port string) {
-    fmt.Println("Setting up server on " + port + " port")
-
-    fmt.Println("Listening...")
-    err := http.ListenAndServe(":" + port, nil)
-
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+    goWebServer.SetupServer("9090")
 }
