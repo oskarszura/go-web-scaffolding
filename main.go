ac3e9a329afb69eb722abc50da62f3328fa8c0d0
oskarszura <oskar.szura@gmail.com>
2016-07-03 19:08:29 +0200

    Deployment test

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
-
 import (
-    goWebServer "github.com/oskarszura/gowebserver"
+    "log"
+    "fmt"
+    "net/http"
+    "os"
 )
-
-var server goWebServer.WebServer
-
+func determineListenAddress() (string, error) {
+    port := os.Getenv("PORT")
+    if port == "" {
+        return "", fmt.Errorf("$PORT not set")
+    }
+    return ":" + port, nil
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello World")
+}
 func main() {
-    server.RunServer("9090")
+    addr, err := determineListenAddress()
+    if err != nil {
+        log.Fatal(err)
+    }
+    http.HandleFunc("/", hello)
+    log.Printf("Listening on %s...\n", addr)
+    if err := http.ListenAndServe(addr, nil); err != nil {
+        panic(err)
+    }
 }
