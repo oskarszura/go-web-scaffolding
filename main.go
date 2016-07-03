a6fbb5066bb77a5242623caaf8479cdb423bcc2d
oskarszura <oskar.szura@gmail.com>
2016-07-03 19:24:19 +0200

    Deployment test

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 import (
-    "log"
     "fmt"
     "net/http"
 )
@@ -9,10 +8,8 @@ func hello(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello World")
 }
 func main() {
-
     http.HandleFunc("/", hello)
-    log.Printf("Listening on %s...\n", 9090)
-    if err := http.ListenAndServe(9090, nil); err != nil {
+    if err := http.ListenAndServe(":9090", nil); err != nil {
         panic(err)
     }
 }
