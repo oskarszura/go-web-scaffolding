70f67996769dad9bd59a49c4cdae1768ece890f1
oskarszura <oskar.szura@gmail.com>
2016-07-03 19:22:18 +0200

    Deployment test

--- main.go
+++ main.go
@@ -3,26 +3,16 @@ import (
     "log"
     "fmt"
     "net/http"
-    "os"
 )
-func determineListenAddress() (string, error) {
-    port := os.Getenv("PORT")
-    if port == "" {
-        return "", fmt.Errorf("$PORT not set")
-    }
-    return ":" + port, nil
-}
+
 func hello(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello World")
 }
 func main() {
-    addr, err := determineListenAddress()
-    if err != nil {
-        log.Fatal(err)
-    }
+
     http.HandleFunc("/", hello)
-    log.Printf("Listening on %s...\n", addr)
-    if err := http.ListenAndServe(addr, nil); err != nil {
-        panic(err) 
+    log.Printf("Listening on %s...\n", 9090)
+    if err := http.ListenAndServe(9090, nil); err != nil {
+        panic(err)
     }
 }
