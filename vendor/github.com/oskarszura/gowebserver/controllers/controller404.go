40e2bd805377079c977e2af9889d95c8c863e7c2
oskarszura <oskar.szura@gmail.com>
2016-07-03 20:05:20 +0200

    Deps update

--- vendor/github.com/oskarszura/gowebserver/controllers/controller404.go
+++ vendor/github.com/oskarszura/gowebserver/controllers/controller404.go
@@ -4,10 +4,19 @@ import (
 	"net/http"
 	"html/template"
 	"github.com/oskarszura/gowebserver/models"
+	"os"
+	"path/filepath"
+	"log"
 )
 
 func Controller404(w http.ResponseWriter, r *http.Request) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	p, _ := models.LoadPage("404")
-	t, _ := template.ParseFiles("public/view.html")
+	t, _ := template.ParseFiles(dir + "/gpublic/view.html")
 	t.Execute(w, p)
 }
--- vendor/github.com/oskarszura/gowebserver/controllers/controllerFront.go
+++ vendor/github.com/oskarszura/gowebserver/controllers/controllerFront.go
@@ -4,10 +4,19 @@ import (
 	"net/http"
 	"html/template"
 	"github.com/oskarszura/gowebserver/models"
+	"os"
+	"path/filepath"
+	"log"
 )
 
 func ControllerFront(w http.ResponseWriter, r *http.Request) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	p, _ := models.LoadPage("index")
-	t, _ := template.ParseFiles("public/view.html")
+	t, _ := template.ParseFiles(dir + "/public/view.html")
 	t.Execute(w, p)
 }
