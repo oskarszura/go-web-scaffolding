b956fa6f535ac1f2f6d5ec78b9d67f310baf149b
oskarszura <oskar.szura@gmail.com>
2017-08-13 12:35:04 +0200

    TRIPS-30 - Changelog generator

---
+++ scripts/changelog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPOSITORY=http://github.com/oskarszura/trips
+NEWER_TAG=$(git tag | tail -r | sed -n 1p)
+OLDER_TAG=$(git tag | tail -r | sed -n 2p)
+HEADER="#Changelog from $NEWER_TAG"
+LOGFILE=./docs/changelogs/CHANGELOG_$NEWER_TAG.md
+
+echo $HEADER > $LOGFILE
+
+git log $OLDER_TAG..$NEWER_TAG --pretty=format:'[[%s]('$REPOSITORY'/commit/%H)]' >> $LOGFILE
