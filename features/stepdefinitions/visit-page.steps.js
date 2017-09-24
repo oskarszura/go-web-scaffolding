e7ac25bed9b8418869c50ccb981806d463c4fcbf
oskarszura <oskar.szura@gmail.com>
2017-09-24 22:37:19 +0200

    TRIPS-38 - Try to adopt cucumber
    * Cover login redirection case with cucumber

--- features/stepdefinitions/visit-page.steps.js
+++ features/stepdefinitions/visit-page.steps.js
@@ -1,20 +1,40 @@
 const Cucumber = require('cucumber');
+const { Builder, By } = require('selenium-webdriver');
 
 Cucumber.defineSupportCode((context) => {
+  const setWorldConstructor = context.setWorldConstructor;
   const given = context.Given;
   const when = context.When;
   const then = context.Then;
 
-  given(/^I'm logged out$/, () => {
+  let url = '';
+
+  const CustomWorld = function () {
+    this.driver = new Builder()
+      .usingServer('http://localhost:4444/wd/hub')
+      .forBrowser('chrome')
+      .build();
+  };
+
+  setWorldConstructor(CustomWorld);
+
+  given(/^I'm not logged in$/, () => {
 
   });
 
-  when(/^I visit front page$/, () => {
-
+  when(/^I visit random page$/, function (callback) {
+    this.driver
+      .get('http://localhost:3222/random-url')
+      .then(() => this.driver.getCurrentUrl())
+      .then((currentUrl) => {
+        url = currentUrl;
+        callback();
+      });
   });
 
-  then(/^I should be redirected to the login page$/, () => {
-
+  then(/^I should be redirected to the login page$/, (callback) => {
+    if (url === 'http://localhost:3222/login') {
+      callback();
+    }
   });
 });
-
--- features/visit-page.feature
+++ features/visit-page.feature
@@ -1,9 +1,10 @@
 Feature: User can access any sub page and be redirected when logged out
   As a user
-  I want to visit any page
-  And be redirected when logged out
+  I want to visit random path
+  And be redirected to the login page
+  When I'm not logged in
 
-  Scenario: Visit front page
-    Given I'm logged out
-    When I visit front page
+  Scenario: Visit random page
+    Given I'm not logged in
+    When I visit random page
     Then I should be redirected to the login page
