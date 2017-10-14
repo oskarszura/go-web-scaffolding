57d1e125f766c1dbc4043bf635c662dde101a098
oskarszura <oskar.szura@gmail.com>
2017-10-14 16:09:19 +0200

    TRIPS-39 - Componentize Elm elements
    * Cleanup main file

--- client/app.jsx
+++ client/app.jsx
@@ -7,18 +7,16 @@ const Elm = require('./Trips/Main.elm');
 const tripNode = document.querySelector('.js-trips');
 
 if (tripNode) {
-  const app = Elm.Trips.Main.embed(tripNode);
+  Elm.Trips.Main.embed(tripNode);
 
-  //app.ports.addTrip.subscribe(() => {});
-
-  //app.ports.openTrip.subscribe((location) => {
-//    console.log(location);
-    /* setTimeout(() => {
-      const mapNode = document.querySelector('.js-map');
-      render(<TripMap
-        places={[]}
-        onAddPlace={app.ports.addPlace.subscribe}
-      />, mapNode);
-    }, 500); */
-//  });
+  // app.ports.addTrip.subscribe(() => {});
+  // app.ports.openTrip.subscribe((location) => {
+  //    setTimeout(() => {
+  //     const mapNode = document.querySelector('.js-map');
+  //     render(<TripMap
+  //       places={[]}
+  //       onAddPlace={app.ports.addPlace.subscribe}
+  //     />, mapNode);
+  // }, 500);
+  // });
 }
