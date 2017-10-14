78a0700e2b26b074f7029f5c4766de81e4e09c9d
oskarszura <oskar.szura@gmail.com>
2017-10-14 17:03:02 +0200

    TRIPS-39 - Componentize Elm elements
    * Wrap elm trips application into react component
    * Minor refactoring

--- client/Trips/Utilities.elm
+++
@@ -1,9 +0,0 @@
-module Trips.Utilities exposing (..)
-
-import Debug exposing (..)
-
-valueFromMaybe : Maybe a -> a
-valueFromMaybe maybeValue =
-  case maybeValue of
-    Just value -> value
-    Nothing -> Debug.crash "Error: valueFromMaybe"
