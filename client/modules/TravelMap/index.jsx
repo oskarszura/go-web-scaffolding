b7a1afe50f0717c514b9d068118a7693c3606b04
Oskar Szura <oskar.szura@gmail.com>
2018-03-12 19:12:16 +0100

    TRIPS-46 - Implement 'travel map' component
    * Display map from google api

--- client/modules/TravelMap/index.jsx
+++ client/modules/TravelMap/index.jsx
@@ -3,11 +3,30 @@ import React, { Component } from 'react';
 const ElmTravelMapApp = require('./Main.elm');
 
 export default class TravelMap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      settings: {
+        center: { lat: 0, lng: 0 },
+        zoom: 1,
+      },
+    };
+  }
+
   componentDidMount() {
-    ElmTravelMapApp.TravelMap.Main.embed(document.querySelector('.js-travel-map'));
+    ElmTravelMapApp.TravelMap.Main.embed(this.mapApp);
+
+    this.map = new google.maps.Map(this.mapContainer, this.state.settings);
   }
 
   render() {
-    return (<div className="js-travel-map" />);
+    return (<div className="travel-map">
+      <div ref={(ref) => { this.mapApp = ref; }} />
+      <div
+        className="travel-map__board"
+        ref={(ref) => { this.mapContainer = ref; }}
+      />
+    </div>);
   }
 }
